5dcc7a6c1e1f8df3f0bf3906773524c92f4c0b9a
ClauAlvarez01 <clau200103am@gmail.com>
2024-03-01 23:03:57 -0500

    feat: Add corpus preprocessing

---
+++ src/code/pre_processing.py
@@ -0,0 +1,76 @@
+import spacy
+import gensim
+import sys
+import os
+import ir_datasets
+ruta_src = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(ruta_src)
+
+def load_corpus():
+    dataset = ir_datasets.load("cranfield")
+    documents = [doc.text for doc in dataset.docs_iter()]
+    return documents
+
+class ProcessingData:
+
+    def __init__(self, nlp):
+        self.nlp = nlp
+        self.documents = load_corpus()
+        self.tokenized_docs = self.process_documents(self.documents)
+        self.dictionary = gensim.corpora.Dictionary(self.tokenized_docs)
+        self.tokenized_docs = self.filter_tokens_by_occurrence(
+            self.tokenized_docs, self.dictionary)
+        self.vocabulary = self.build_vocabulary()
+        self.vector_repr = self.vector_representation()
+
+    def process_documents(self, documents):
+        tokenized_docs = self.tokenization_spacy(documents)
+        tokenized_docs = self.remove_noise_spacy(tokenized_docs)
+        tokenized_docs = self.remove_stopwords_spacy(tokenized_docs)
+        tokenized_docs = self.morphological_reduction_spacy(tokenized_docs)
+        
+        return tokenized_docs
+
+    # Tokenización
+    def tokenization_spacy(self, texts):
+        return [[token for token in self.nlp(doc)] for doc in texts]
+
+    # Eliminación de Ruido
+    def remove_noise_spacy(self, tokenized_docs):
+        return [[token for token in doc if token.is_alpha] for doc in tokenized_docs]
+
+    # Eliminación de Stop-Words
+    def remove_stopwords_spacy(self, tokenized_docs):
+        stopwords = spacy.lang.en.stop_words.STOP_WORDS
+        return [[token for token in doc if token.text.lower() not in stopwords] for doc in tokenized_docs]
+
+    # Reducción Morfológica
+    def morphological_reduction_spacy(self, tokenized_docs):
+        return [[token.lemma_ for token in doc] for doc in tokenized_docs]
+
+    # Filtrado según ocurrencia
+    def filter_tokens_by_occurrence(self, tokenized_docs, dictionary, no_below=5, no_above=0.5):
+        dictionary.filter_extremes(no_below=no_below, no_above=no_above)
+        filtered_words = [word for _, word in dictionary.iteritems()]
+        return [[word for word in doc if word in filtered_words] for doc in tokenized_docs]
+
+    # Construcción del vocabulario
+    def build_vocabulary(self):
+        return list(self.dictionary.token2id.keys())
+
+    # Representación vectorial de Docs
+    def vector_representation(self):
+        corpus = [self.dictionary.doc2bow(doc) for doc in self.tokenized_docs]
+
+        tfidf = gensim.models.TfidfModel(corpus)
+        vector_repr = [tfidf[doc] for doc in corpus]
+
+        return vector_repr
+
+    # #Etiquetado
+    # def pos_tagger_spacy(self):
+    #     return [[(token.text, token.tag_) for token in doc] for doc in self.tokenized_docs]
+    # def pos_tagger_spacy(tokenized_docs):
+    #     return [[(token.text, token.tag_) for token in doc] for doc in tokenized_docs]
+
+
---
+++ src/code/setup.py
@@ -0,0 +1,39 @@
+import spacy
+import sys
+import os
+import json
+from pre_processing import ProcessingData
+ruta_src = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(ruta_src)
+
+nlp_instance=spacy.load("en_core_web_sm")
+
+processing_data = ProcessingData(nlp_instance)
+
+documents = {i + 1: doc for i, doc in enumerate(processing_data.documents)}
+
+documents_per_token = {}
+for index, token in enumerate(processing_data.tokenized_docs):
+    for item in token: 
+        if item in documents_per_token:
+            if not (index+1) in documents_per_token[item]:
+                documents_per_token[item].append(index+1)
+        else:
+            documents_per_token[item] = [index+1]
+
+ 
+def save_documents_to_json(text, json_file_name, file='data'):
+        json_path = os.path.join(file, json_file_name)
+        file_name = json_file_name.split('.')
+        data = {file_name[0]: text}
+
+        try:
+            json_string = json.dumps(data, indent=2)
+            with open(json_path, 'w', encoding='utf-8') as file_json:
+                file_json.write(json_string)
+                print(f"La exportación a {json_path} se realizó con éxito.")
+        except Exception as e:
+            print(f"Error al exportar a JSON: {e}")
+
+save_documents_to_json(documents, 'documents.json', file='data')
+save_documents_to_json(documents_per_token, 'docs_per_token.json', file='data')
