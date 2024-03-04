234171a8528a60b41d17f992667b916041eb8da4
Roger <morenoroger001@gmail.com>
2024-03-03 19:06:39 -0500

    feat: Personalize dashboard

--- src/gui/src/Dashboard.tsx
+++ src/gui/src/Dashboard.tsx
@@ -2,23 +2,20 @@ function Dashboard() {
     return (
         <>
             <div className="min-h-full">
-                <nav className="bg-gray-800">
-                    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                        <div className="flex h-16 items-center justify-between">
-                            <div className="flex items-center">
-                                <div className="flex-shrink-0">
-                                    <img className="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
-                                </div>
-                                <div className="">
-                                    <div className="ml-10 flex items-baseline space-x-4">
-                                        <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
-                                        {/* <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Team</a> */}
+
+                <div className="shadow-xl">
+                    <nav className="bg-sky-300 shadow-xl">
+                        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 sha">
+                            <div className="flex h-16 items-center justify-between">
+                                <div className="flex items-center">
+                                    <div className="flex-shrink-0">
+                                        <img className="h-10 mt-1" src="logo.png" alt="Your Company" />
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </nav>
+                    </nav>
+                </div>
 
                 <header className="bg-white shadow">
                     <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
